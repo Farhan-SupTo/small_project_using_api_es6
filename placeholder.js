baeb916e4b3ef93fe0115facea04e18b56c59439
Farhan SupTo <farhansupto1996@gmail.com>
2023-09-24 13:11:31 +0600

    json placeholder practise using api updated

--- placeholder.js
+++ placeholder.js
@@ -1,3 +1,4 @@
+let count =0;
 const loadData = () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then((response) => response.json())
@@ -10,11 +11,13 @@ const loadData = () => {
 const showData = (data) => {
     for(let singleData of data.slice(0, 20)){
         // console.log(singleData);
-
+        
+        count +=1;
         const container = document.getElementById("info")
         const div =document.createElement("div")
         div.innerHTML =`
-        <h1 class="text-3xl text-center font-semibold">${singleData.title}</h1>
+        
+        <h1 class="text-3xl text-center font-semibold p-2">${count}. ${singleData.title}</h1>
         `;
 
         container.appendChild(div);
