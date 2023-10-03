0367c7d6ffbc219fa95e285be1d21ac5c04d30de
Your Name~ <you@example.com>
2023-10-03 16:42:15 +0600

    add banner on right site

--- sportsdb/sports.js
+++ sportsdb/sports.js
@@ -1,5 +1,8 @@
 const searchAllData = () =>{
 const inputELement =document.getElementById("search-value")
+document.getElementById("singlePlayerDetails").innerHTML =""
+document.getElementById("male").classList.add("d-none")
+document.getElementById("female").classList.add("d-none")
 const inputValue =inputELement.value
 // console.log(inputValue);
 const URL =`https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${inputValue}`
@@ -10,6 +13,7 @@ fetch(URL)
 
 const showPlayerData =(players) =>{
     const container =document.getElementById("player-info")
+    document.getElementById("search-value").value =""
     container.innerHTML =""
 players.forEach(player => {
     console.log(player);
@@ -20,12 +24,52 @@ players.forEach(player => {
       <div class="card-body">
         <h5 class="card-title">${player.strPlayer}</h5>
         <p class="card-text">Nationality :${player.strNationality }</p>
-        <div class="d-flex flex-row my-2">
-        <button type="button" class="btn btn-primary">Details</button></div>
-        <button type="button" class="btn btn-danger">Delete</button></div>
+        <div class="my-2">
+        <button onclick="singlePlayer('${player.idPlayer}')" type="button" class="btn btn-primary">Details</button>
+        <button type="button" class="btn btn-danger">Delete</button>
+        </div>
       </div>
     `
  container.appendChild(div)
 });
     
+}
+
+const singlePlayer = (id) =>{
+        //  console.log(id);
+         const URL =`https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=${id}`
+       
+         fetch(URL)
+         .then(res => res.json())
+         .then(data => showSinglePlayerData(data.players[0]))
+}
+const showSinglePlayerData =(data) =>{
+  console.log(data);
+       const container =document.getElementById("singlePlayerDetails")
+       const div =document.createElement("div")
+       if (data.strGender === "Male"){
+        const element =document.getElementById("male")
+        element.classList.remove("d-none")
+       }else{
+        const element =document.getElementById("female")
+        element.classList.remove("d-none")
+       }
+       div.innerHTML =`
+       <div class="card mb-3 h-100 w-100 my-2">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${data.strThumb}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${data.strPlayer}</h5>
+        <p class="card-text">${data.strDescriptionEN.slice(0, 80) + "..."}</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+       `
+
+       container.appendChild(div)
 }
